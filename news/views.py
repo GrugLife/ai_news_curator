c9c7dafc87bcb401e57f26b36017ade83ab8522e
GrugLife <gwylie1@gmail.com>
2025-12-21 17:22:14 -0800

    add views and templates

--- news/views.py
+++ news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Article
+
+
+def article_list(request):
+    # fetch all articles, ordered by the most recent first
+    articles = Article.objects.all().order_by("-created_at")
+    # pass the articles to the template using a context dictionary
+    return render(request, "news/article_list.html", {"articles": articles})
+
 
 # Create your views here.
